9b89a70841d3322721d27dd7ba498d618f293b97
alethrome <49285810+alethrome@users.noreply.github.com>
2021-08-29 18:31:18 +0800

    Delete task

--- App.js
+++ App.js
@@ -13,6 +13,12 @@ export default function App() {
     setTask(null)
   }
 
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1)
+    setTaskItems(itemsCopy)
+  }
+
   return (
     <View style={styles.container}>
       {/* Today's Tasks */}
@@ -22,7 +28,11 @@ export default function App() {
           {/* This is where the tasks will go! */}
           {
             taskItems.map((item, index) => {
-              return <Task key={index} text={item} />
+              return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task text={item} />
+                </TouchableOpacity>
+              )
             })
           }
         </View>
